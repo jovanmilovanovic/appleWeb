bfa8da034bc7ee5b448a28f7ce9cab4fed2565c7
jovanmilovanovic <pressingreklame034@gmail.com>
2025-01-12 22:23:50 +0100

    Install sentry for my project

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://bf3a74c779488c9536a79d114573d7a0@o4508632754225152.ingest.de.sentry.io/4508632769364048",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+ 
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
